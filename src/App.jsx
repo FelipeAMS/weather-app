596ce0cb72961d6696adae4155f75a55e2709d5e
FelipeAMS <xfoarmks@gmail.com>
2025-05-08 13:35:51 -0500

    fixing router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Router basename="/weather-app">
-        <div className="app">
-          <Routes>
-            <Route path="/" element={<Weather />} />
-
-            <Route path="*" element={<div>404 - Page Not Found</div>} />
-          </Routes>
-        </div>
-      </Router>
+    <Router basename="/weather-app">
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Weather />} />
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
